7752a9cef2f05944ee6695bc99ddc23d5701e3b4
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-04-02 23:41:50 +0530

    Axios

---
+++ Axios/Another part/app.js
@@ -0,0 +1,30 @@
+//Same thing we could do for our dog API
+let btn = document.querySelector("button");
+let url2 = "https://dog.ceo/api/breeds/image/random";
+
+btn.addEventListener("click", async ()=>{
+    // let fact = await getDogImg();
+    // console.log(fact);
+    // let p = document.querySelector("#result");
+    // p.innerText = fact;
+    let link = await getDogImg();
+    console.log(link);
+    let img = document.querySelector("#result");
+    img.setAttribute("src", link);
+});
+
+async function getDogImg(){
+    try{
+    let res = await axios.get(url2);
+    // return res.data.fact;
+    return res.data.message;
+    }
+    catch(err){
+        // console.log("Error: ", err);
+        return "NO Image Found";
+    }
+}
+
+getDogImg();
+
+

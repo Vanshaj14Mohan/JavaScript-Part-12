819895b5ed16e5555eeb2a77e8d8d3adcef1901e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-04-03 17:42:40 +0530

    Axios

---
+++ Headers with API/app.js
@@ -0,0 +1,15 @@
+const url = "https://icanhazdadjoke.com/";
+
+async function getJokes(){
+    try{
+        const config = {headers: {Accept: "application/json"}};
+        let res = await axios.get(url, config);
+        //console.log(res.data); //it will return the data in HTML format. But we want it to be in application json format
+        console.log("Joke found: ", res.data.joke);
+    }
+    catch (err){
+        console.log("Error found", err);
+    }
+}
+
+getJokes();
